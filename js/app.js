71bc7ad8511bbac60da6e5d774872d50b8146192
ashutosh-ve <axxuverma@gmail.com>
2021-11-16 20:31:10 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.f-slider').slick({
+        slidesToShow: 3,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn",
+        autoplay: true,
+        
+
+
+    });
+});
